77fd30053df86131e6027553265ad37a1f11f0d3
Marigrace Seaton <marigraceseaton@gmail.com>
2019-12-06 02:34:58 -0500

    woooo

--- script.js
+++ script.js
@@ -105,3 +105,59 @@ window.onclick = function(event) {
         anneModal.style.display == "none"; 
     }
 }
+
+// VICTORIA FUNCTIONALITY 
+let victoriaModal = document.getElementById("victoriaModal"); 
+let victoriaButton = document.getElementById("victoriaButton"); 
+let victoriaClose = document.getElementById("victoriaClose"); 
+victoriaButton.onclick = function() {
+    victoriaModal.style.display = "block"; 
+}
+victoriaClose.onclick = function() {
+    victoriaModal.style.display = "none"; 
+}
+window.onclick = function(event) {
+    if (event.target == victoriaModal) {
+        victoriaModal.style.display == "none"; 
+    }
+}
+
+// ELIZABETH II FUNCTIONALITY 
+let eliziiModal = document.getElementById("elizabethiiModal"); 
+let eliziiButton = document.getElementById("elizabethiiButton"); 
+let eliziiClose = document.getElementById("elizabethiiClose"); 
+eliziiButton.onclick = function() {
+    eliziiModal.style.display = "block"; 
+}
+eliziiClose.onclick = function() {
+    eliziiModal.style.display = "none"; 
+}
+window.onclick = function(event) {
+    if (event.target == eliziiModal) {
+        eliziiModal.style.display == "none"; 
+    }
+}
+
+// TAB FUNCTIONALITY 
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
